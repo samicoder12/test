4880ed459583f1b7629ed4e2ec533991cd025f23
samicoder12 <somsonmisrak777@gmail.com>
2024-08-28 15:18:23 +0300

    this is first commit

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        neutralSilver: "#F5F7FA",
+        neutralDGray: "#4D4D4D",
+        brandPrimary: "#4ACF4F",
+        neutralGray: "#717171",
+
+      }
+    },
+  },
+  plugins: [],
+}
+
